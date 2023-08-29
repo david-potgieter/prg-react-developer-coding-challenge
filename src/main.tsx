09ecdcff1ebac125ab7b1a048a3597ba63b18243
David Potgieter <david@weswingcats.com>
2023-08-29 10:58:07 +0700

    tweaks and config changes, added deps

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
+import { router } from '@core/config/routes'
+import '@core/styles/index.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import { RouterProvider } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <RouterProvider router={router} />
+  </React.StrictMode>
 )
