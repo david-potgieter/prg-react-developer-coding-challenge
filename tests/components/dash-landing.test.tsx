21e0b969934d855e52357decdeb8174d161746b8
David Potgieter <david@weswingcats.com>
2023-08-30 16:16:42 +0700

    extended wraper

--- tests/components/dash-landing.test.tsx
+++ tests/components/dash-landing.test.tsx
@@ -1,12 +1,21 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { render, screen } from '@testing-library/react'
 import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import { describe, expect, it } from 'vitest'
 import { DashboardLayout } from '../../src/components/layouts/dashboard-layout'
+
+const queryClient = new QueryClient()
+
+const wrapper = ({ children }) => (
+  <QueryClientProvider client={queryClient}>
+    <BrowserRouter>{children}</BrowserRouter>{' '}
+  </QueryClientProvider>
+)
 describe('App', () => {
   it('renders headline', () => {
     render(<DashboardLayout title="Testing Layout">Dash</DashboardLayout>, {
-      wrapper: BrowserRouter,
+      wrapper,
     })
     const title = screen.getByText(/Testing Layout/i)
     expect(title).toBeInTheDocument()
