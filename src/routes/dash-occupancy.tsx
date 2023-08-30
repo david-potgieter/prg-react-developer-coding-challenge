27c9eef7e609ec73b6490f427e66d06664038c71
David Potgieter <david@weswingcats.com>
2023-08-30 14:17:27 +0700

    deps, updates and tweaks

--- src/routes/dash-occupancy.tsx
+++ src/routes/dash-occupancy.tsx
@@ -1,7 +1,14 @@
 import { DashboardLayout } from '@core/components/layouts/dashboard-layout'
 import { useRouteConfig } from '@core/state/hooks/use-route-config'
+import { OccupancyGraph } from '../components/ui/chart/occupancy-graph'
 
 export default function DashOccupancy() {
   const route = useRouteConfig()
-  return <DashboardLayout title={route?.name}>Dashboard occupancy</DashboardLayout>
+  return (
+    <DashboardLayout title={route?.name}>
+      <div className="w-full h-screen max-h-72">
+        <OccupancyGraph />
+      </div>
+    </DashboardLayout>
+  )
 }
